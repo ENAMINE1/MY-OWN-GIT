c88aec6828f2e9ba334ca38f0b730bc6e21c08df
akrisfx <vpotach@gmail.com>
2024-06-28 01:26:49 +0300

    new func checking perms

--- src/Headers/write_tree.h
+++ src/Headers/write_tree.h
@@ -4,8 +4,10 @@
 
 #include <string>
 #include <vector>
+#include <filesystem>
 
 std::string write_tree(const std::string &directory);
+std::string check_file_perms(std::filesystem::perms file_perms);
 // std::vector<Entry> read_index();
 
 #endif // WRITE_TREE_H
